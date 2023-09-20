96c1fe8ecfe4be090d5a3cf66bb3bec7f0b7c3f0
Juanky <juancamiloherrera960@gmail.com>
2023-09-19 19:45:58 -0500

    Update general

--- script.js
+++ script.js
@@ -2,36 +2,39 @@
 //----------------------------------
 // 1. Entrada del programa Input (I)
 //----------------------------------
-var items = [2, 3, 5, 7, 11, 13, 17]; // index es el cursor
-console.log("en la posición 3 tenemos: " + items[1])
+const items = [2, 3, 5, 7, 11, 13, 17]; // index es el cursor
 //----------------------------------
 // 2. Definición función de búsqueda
 //----------------------------------
-// a la función de busqueda le vamos a pasar dos parametros: el array items y el elemento_a_buscar y el objetivo del algoritmo es encontrar en que posición está el elemento_a_buscar
+// A la función de busqueda le vamos a pasar dos parametros: el array items y el elemento_a_buscar y el objetivo del algoritmo es encontrar en que posición está el elemento_a_buscar
 function search(items, elemento_a_buscar) { 
-  var posicion = 0;
-  var total_itemsArray = items.length;
+  let posicion = 0;
+  const total_itemsArray = items.length;
+
   while (posicion < total_itemsArray) {
     if (items[posicion] == elemento_a_buscar) { 
       return posicion;
+
     }
-    posicion++; // sumar de a un
+    posicion++; // sumar 1 a la variable
   }
   return null;
 }
 //-----------------------------------------------------------
 // 3. Ejecución de la función: buscar un elemento en el array
 //-----------------------------------------------------------
-var resultado = search(items, 13);
-console.log(resultado)
+const resultado = search(items, 13);
 //----------------------------------
 // 4. Salida del programa Output (O)
 //----------------------------------
-var resultadoHTML = document.createElement('p');
+const container = document.querySelector('#container');
+const resultadoHTML = document.createElement('p');
 resultadoHTML.id = 'resultado'; // Asigna el ID que desees
+
 if (resultado !== null) {
-  resultadoHTML.textContent = 'El elemento se encuentra en la posición: ' + resultado + ' :)';
+  resultadoHTML.textContent = `El elemento se encuentra en la posición: ${resultado} \u{1F643}`;
 } else {
-  resultadoHTML.textContent = 'El elemento no se encuentra en el array :(';
+  resultadoHTML.textContent = 'El elemento no se encuentra en el array \u{1F644}';
 }
-document.body.appendChild(resultadoHTML);
+
+container.appendChild(resultadoHTML);
