d84e07b3e3bd4b81a07bd112066cb0bfce834720
Juanky <juancamiloherrera960@gmail.com>
2023-09-29 21:07:00 -0500

    Menu, foreach

--- foreach.js
+++ foreach.js
@@ -10,3 +10,19 @@ function contarEstrellas(galaxies) {
 
 var result = contarEstrellas(galaxies);
 document.querySelector("#header").innerHTML = result;
+
+
+const letters = ["a", "b", "c", "d"];
+
+const f = function () {
+  console.log("Un elemento de función por expresión");
+};
+letters.forEach(f);
+
+
+letters.forEach(function () {
+  console.log("Un elemento de función anónima");
+});
+
+
+letters.forEach(() => console.log("Un elemento de función flecha"));
